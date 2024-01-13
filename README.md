0383a6c3ef6485bb15ddaddc75ca30a55bd32cca
Cosmitar <mhherrera31@gmail.com>
2024-01-13 03:20:01 -0300

    add nipple joystick + buttons

--- README.md
+++ README.md
@@ -1,30 +1,41 @@
-# React Three Fiber + Vite Starter
+# Attribution for models
 
-_A simple React Three Fiber + Vite starter with an import-ready CodeSandbox config._
+SCHOOL BUS by Kyle Li [CC-BY] via Poly Pizza
+Shipping Container by Quaternius
+Shipping Container by Quaternius
 
-![Preview](/public/preview.gif)
+# Art Assets
 
-### Resources:
+[OpenGameArt (2D + 3D)](https://opengameart.org/)
+[Kenney (2D + 3D)](https://www.kenney.nl/assets)
+[Game-Icons (2D)](https://game-icons.net/)
+[Textures.com (Textures)](https://www.textures.com/)
+[Poliigon (Textures)](https://www.poliigon.com/search?credit=0)
+[Poly.pizza (3D)](https://poly.pizza/)
+[Mixamo (Animations)](https://www.mixamo.com/)
 
-- [React Three Fiber](https://docs.pmnd.rs/react-three-fiber/): Three.js Renderer
-- [Drei](https://github.com/pmndrs/drei): React Three Fiber Helpers
-- [Three.js](https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene): 3D Engine
-- [Vite](https://vitejs.dev/guide/): Static Web Server
-- [CodeSandbox](https://codesandbox.io/docs/configuration): Online Prototyping Container (optional)
+# Fonts
 
-### Installation
+[Fontsource (Fonts)](https://github.com/fontsource/fontsource)
+[Font Squirrel (Fonts)](https://www.fontsquirrel.com/)
+[Google Fonts (Fonts)](https://fonts.google.com/)
 
-```
-npm install
-```
+# Audio
 
-### Scripts
+[Incompetech - Royalty Free Music (Music)](https://incompetech.com/music/royalty-free/)
+[Free Music Archive (Music)](https://freemusicarchive.org/)
+[Freesound (SFX)](https://freesound.org/)
+[OpenGameArt (Music + SFX)](https://opengameart.org/)
+[Kenney (Music + SFX)](https://www.kenney.nl/assets?q=audio)
+[Soniss (SFX)](https://sonniss.com/gameaudiogdc)
 
-```
-npm run dev
-npm run build
-npm run preview
-npm run sandbox
-```
-### CodeSandbox
-To create a sandbox from this starter, [import your repository URL](https://codesandbox.io/dashboard/repositories) (this repo works too), or [fork this template](https://codesandbox.io/s/react-three-fiber-vite-starter-r1tgld).
+# Tools
+
+[BitFontMaker2 (Fonts)](http://www.pentacom.jp/pentacom/bitfontmaker2/)
+[Photopea (Art)](https://www.photopea.com/)
+[sfxr (SFX)](https://www.drpetter.se/project_sfxr.html)
+[Chiptone (SFX)](https://sfbgames.itch.io/chiptone)
+[Bosca Ceoil (Music)](https://boscaceoil.net/)
+[BeepBox (Music)](https://www.beepbox.co/)
+[Lospec (Palettes)](https://lospec.com/palette-list)
+[Tiny Tools (More!)](https://tinytools.directory/)
--- src/systems/VirtualJoystickSystem/VirtualJoystickSystem.tsx
+++ src/systems/VirtualJoystickSystem/VirtualJoystickSystem.tsx
@@ -1,16 +1,84 @@
-import { useLayoutEffect } from 'react'
+import { useLayoutEffect, useState } from 'react'
 import nipplejs from 'nipplejs'
+import { useGame, useJoystickControls } from '../../utils/Ecctrl/Ecctrl'
+import { MathUtils } from 'three'
+import jumpButtonImage from '../../../public/assets/icons/jump-icon.svg'
+import waveButtonImage from '../../../public/assets/icons/say_hi-icon.svg'
 
 export default function VirtualJoystickSystem() {
+  const setJoystick = useJoystickControls(state => state.setJoystick)
+  const resetJoystick = useJoystickControls(state => state.resetJoystick)
+  const jumpAction = useJoystickControls(state => state.pressButton1)
+  const waveAction = useGame(state => state.action1)
+  const releaseAllButtons = useJoystickControls(state => state.releaseAllButtons)
+
   useLayoutEffect(() => {
     const joystickZone = document.querySelector('#joystick_zone') as HTMLElement
-    joystickZone.addEventListener('touchstart', (e) => e.stopPropagation())
-    joystickZone.addEventListener('touchmove', (e) => e.stopPropagation())
+    const btnsZone = document.querySelector('#buttons_zone') as HTMLElement
+    // prevents context menu
+    joystickZone.addEventListener('contextmenu', function (event) {
+      event.preventDefault()
+    })
+    // prevents move camera on drag joystick
+    joystickZone.addEventListener('touchstart', e => e.stopPropagation())
+    joystickZone.addEventListener('touchmove', e => e.stopPropagation())
+
+    // add buttons
+    const createButton = ({
+      id,
+      icon,
+      main = false,
+      action = () => {},
+    }: {
+      id: string
+      icon: string
+      main?: boolean
+      action?: () => void
+    }) => {
+      const btn = document.createElement('a')
+      btn.setAttribute('id', id)
+      btn.setAttribute('class', `joystickButton ${main && 'main'}`)
+
+      btn.addEventListener('touchmove', e => e.stopPropagation())
+      const img = document.createElement('img')
+      img.setAttribute('src', icon)
+      btn.appendChild(img)
+      btnsZone.appendChild(btn)
+      btn.addEventListener('touchstart', e => {
+        e.stopPropagation()
+        action()
+      })
+      btn.addEventListener('touchend', e => {
+        e.stopPropagation()
+        releaseAllButtons()
+      })
+      btn.addEventListener('contextmenu', function (event) {
+        event.preventDefault()
+      })
+      return btn
+    }
+
+    createButton({id: 'wave', icon: waveButtonImage, action: waveAction })
+    createButton({id: 'jump', icon: jumpButtonImage, action: jumpAction, main: true })
+    
+
+    // creates joystic
     const joystick = nipplejs.create({
       zone: joystickZone,
-      color: 'blue',
+      color: '#fffa',
+      mode: 'static',
+      position: {
+        top: '50%',
+        left: '50%'
+      }
     })
-    
+
+    // connects Nipplejs to Ecctrl joystick
+    joystick.on('end', resetJoystick)
+    joystick.on('move', (e, data) => {
+      setJoystick(MathUtils.mapLinear(data.distance, 0, 50, 0, 1), data.angle.radian, data.distance >= 37)
+    })
+
     return () => joystick.destroy()
   }, [])
 
